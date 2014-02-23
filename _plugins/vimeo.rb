069ae81b79f03c9608f833fda1dd9d89e18d9747
lealbaugh <l.e.albaugh@gmail.com>
2014-02-22 20:47:31 -0500

    Fluid embedded Vimeo videos.

---
+++ _plugins/vimeo.rb
@@ -0,0 +1,22 @@
+# A plugin for embedding videos from Vimeo using a simple Liquid tag, ie: {% vimeo 12345678 %}.
+# Based of the Youtube plugin from http://www.portwaypoint.co.uk/jekyll-youtube-liquid-template-tag-gist/
+
+# Modified for fluid-width videos based on http://css-tricks.com/NetMag/FluidWidthVideo/Article-FluidWidthVideo.php
+
+module Jekyll
+  class Vimeo < Liquid::Tag
+    @@width = 500
+    @@height = 281
+
+    def initialize(name, id, tokens)
+      super
+      @id = id
+    end
+
+    def render(context)
+      %(<div class="videoWrapper"><iframe width="#{@@width}" height="#{@@height}" src="http://player.vimeo.com/video/#{@id}" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>)
+    end
+  end
+end
+
+Liquid::Template.register_tag('vimeo', Jekyll::Vimeo)
